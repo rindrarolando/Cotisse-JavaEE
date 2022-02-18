e7e807f18c0cbb77f4d5b0b4343adf54f89318be
Rindra Rolando <rindrarolando7@gmail.com>
2022-02-18 10:56:34 +0300

    INIT

---
+++ src/main/java/com/rindra/hopital/ProjetHopitalApplication.java
@@ -0,0 +1,13 @@
+package com.rindra.hopital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetHopitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetHopitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rindra/hopital/ProjetHopitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rindra.hopital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetHopitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
